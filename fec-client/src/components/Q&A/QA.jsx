03eda5e4d37504af952c7220d4a8bb7245ddb464
Ian Hoffman <ianrhoffman1@gmail.com>
2022-03-31 10:12:21 -0700

    Creates components for each app component, adds jest

---
+++ fec-client/src/components/Q&A/QA.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import reactDOM from 'react-dom'
+import styled from 'styled-components'
+
+const QA = function() {
+   return (
+     <div>This is the QA section</div>
+   )
+};
+
+export default QA;
--- fec-client/src/index.jsx
+++ fec-client/src/index.jsx
@@ -2,22 +2,60 @@ import React from 'react'
 import reactDOM from 'react-dom'
 import styled from 'styled-components'
 
+import Products from './components/Related/index.jsx';
+import Ratings from './components/Ratings/index.jsx';
+import Overview from './components/Overview/index.jsx';
+import QA from './components/Q&A/QA.jsx';
+
 const H1 = styled.h1`
   font-size: 2.0em;
   text-align: center;
   color: black;
-`
+`;
+const OverviewDiv = styled.div`
+  border: 1px solid black;
+  width: 90%;
+  height: 25%;
+`;
+
+const  RelatedDiv = styled.div`
+border: 1px solid black;
+width: 90%;
+height: 25%;
+`;
+
+const QandADiv = styled.div`
+border: 1px solid black;
+width: 90%;
+height: 25%;
+`;
+
+const RatingsDiv = styled.div`
+border: 1px solid black;
+width: 90%;
+height: 25%;
+`;
 
 const App = () => {
   return (
   <div>
+    <div>
     <H1>Project Atelier</H1>
-    <div></div>
-    <div>Hello World!</div>
-    <div></div>
-    <div>Goodbye World!</div>
-    <div></div>
-    <div>Poop World!</div>
+  </div>
+  <div>
+    <OverviewDiv>
+      <Overview/>
+    </OverviewDiv>
+    <RelatedDiv>
+        <Products/>
+    </RelatedDiv>
+    <QandADiv>
+        <QA />
+    </QandADiv>
+    <RatingsDiv>
+      <Ratings/>
+    </RatingsDiv>
+  </div>
   </div>
   )
 }
