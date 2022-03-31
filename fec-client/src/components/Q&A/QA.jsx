3dcba0b8ca1c52be464ed67c0ead55de72420326
Owen Yoshishige <owenyoshishige@gmail.com>
2022-03-31 15:21:50 -0700

    save progress to install axios on main

--- fec-client/src/components/Q&A/QA.jsx
+++ fec-client/src/components/Q&A/QA.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 // import styled from 'styled-components';
 
-const QA = function () {
+const QA = function (productId = []) {
+  const [QAData, SetQA] = useState(productId);
+
+  //
+
   return (
-    <div>This is the QA section</div>
+    <div>
+      <div>SearchBar Goes Here</div>
+      <div>QAList Goes Here</div>
+      <div>
+        <button>More Answered Questions</button>
+        <button>Add A Question</button>
+      </div>
+    </div>
   );
 };
 
