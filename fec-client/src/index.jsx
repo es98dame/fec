5f5d6566dd6cba719fe3c738dfcabba85031bf5e
Ian Hoffman <ianrhoffman1@gmail.com>
2022-03-31 09:49:28 -0700

    adds styled components.

--- fec-client/src/index.jsx
+++ fec-client/src/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import reactDOM from 'react-dom'
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+  font-size: 2.0em;
+  text-align: center;
+  color: black;
+`
 
 const App = () => {
   return (
-  <div>Hello World!</div>
-  <div></div>
-  <div>Hello World!</div>
-  <div></div>
-  <div>Goodbye World!</div>
-  <div></div>
-  <div>Poop World!</div>
+  <div>
+    <H1>Project Atelier</H1>
+    <div></div>
+    <div>Hello World!</div>
+    <div></div>
+    <div>Goodbye World!</div>
+    <div></div>
+    <div>Poop World!</div>
+  </div>
   )
 }
 
