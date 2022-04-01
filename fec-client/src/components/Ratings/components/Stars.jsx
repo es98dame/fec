a512772f9b8db5e8745c44e585e542017618c51d
Kate Anderson <kate.anderson250@gmail.com>
2022-04-01 12:42:30 -0700

    Changes the colors and outline of the stars in Stars.jsx

--- fec-client/src/components/Ratings/components/Stars.jsx
+++ fec-client/src/components/Ratings/components/Stars.jsx
@@ -30,11 +30,12 @@ const Stars = ({rating}) => {
         <Star viewBox="0 0 51 48">
           <defs>
             <linearGradient id = {`gradient-${fill}`}>
-              <stop offset = {`${fill}%`} stop-color = "black"></stop>
-              <stop offset = {`${fill}%`} stop-color = "white"></stop>
+              <stop offset = {'0%'} stopColor = "#3D463D"></stop>
+              <stop offset = {`${fill}%`} stopColor = "#3D463D"></stop>
+              <stop offset = {`${fill}%`} stopColor = "#BAC3BA"></stop>
             </linearGradient>
           </defs>
-          <path fill={`url(#gradient-${fill})`} stroke="#000" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"> </path>
+          <path fill={`url(#gradient-${fill})`} stroke="white" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"> </path>
         </Star>
       ))}
     </div>
