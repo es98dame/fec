fb39fad12be9d766951dfb9862b7146627811e02
Owen Yoshishige <owenyoshishige@gmail.com>
2022-05-07 23:02:24 -0700

    Updated exampleConfig.js with correct information.

--- server/config/exampleConfig.js
+++ server/config/exampleConfig.js
@@ -1,12 +1,8 @@
 //copy this code into a file named config.js and fill in with your own api
 
-
 // Put your API key here!
-var API_KEY = 'YOUR_API_KEY_HERE';
+const API_KEY = 'YOUR_API_KEY_HERE';
+const CLOUDINARY = 'YOUR_CLOUDINARY_KEY_HERE';
 
-module.exports = {API_KEY};
-
-router.get('/products', overview.getProducts);
-router.get('/products/:productId', overview.getProductById);
-router.get('/products/:productId/styles', overview.getStyles);
-router.get('/products/:productId/related', overview.getRelated);
+module.exports.CLOUDINARY = CLOUDINARY;
+module.exports.API_KEY = API_KEY;
